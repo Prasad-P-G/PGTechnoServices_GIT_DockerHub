e07b19ead280091ce10d96937322f5df90643898
Prasad-P-G <prasad.dwd@gmail.com>
2024-11-05 22:33:01 +0530

    Products css

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -2,17 +2,17 @@ import React from "react";
 
 const ProductGrid = ({ icon, text, label, heading }) => {
   return (
-    <div className="flex flex-col lg:flex-row gap-2 lg:w-[90%]  items-center w-full bg-slate-200 rounded-lg p-4">
+    <div className="flex flex-col lg:flex-row gap-2 lg:gap-2 items-center lg:justify-start w-full bg-slate-200 rounded-lg p-4">
       <img
         src={icon}
         alt="product image"
-        className="bg-green-400 rounded-full w-[75%]  hover:bg-green-500 cursor-pointer"
+        className="bg-green-400 rounded-full w-[75%] lg:w-[20%] mx-2 hover:bg-green-500 cursor-pointer"
       ></img>
 
-      <h1 className=" w-full text-xl lg:text-2xl text-green-600 font-semibold ">
+      <span className=" text-lg w-full lg:w-[30%] text-center lg:text-2xl rounded-b-2xl lg:rounded-r-2xl text-white font-semibold bg-gray-700 opacity-1 p-3">
         {heading}
-      </h1>
-      <h1 className="text-[18px] text-center text-slate-500">{text}</h1>
+      </span>
+      <h1 className="text-[18px] w-full lg:w-[60%]  text-slate-500">{text}</h1>
       {/* <p className="bg-green-800 text-white px-4 py-2 rounded-md hover:bg-black hover:text-white cursor-pointer">
         {label}
       </p> */}
